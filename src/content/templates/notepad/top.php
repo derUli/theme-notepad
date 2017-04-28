7306adeedb6bf33e506bb01fa630542b2d4c5bc6
Ulrich Schmidt <u.schmidt@velian.de>
2017-04-28 15:22:41 +0200

    metadata.json hinzugefügt

--- src/content/templates/notepad/top.php
+++ src/content/templates/notepad/top.php
@@ -1,7 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+html5_doctype ();
+og_html_prefix ();
+?>
 <head>
-<?php base_metas();?>
+  <?php
+		base_metas ();
+		og_tags ();
+		?>
 <title><?php title()?> - <?php homepage_title()?></title>
 </head>
 
